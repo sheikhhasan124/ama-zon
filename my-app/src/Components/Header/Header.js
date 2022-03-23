744ef7b66f65872d63fc1c0684a252650a62b47f
hasan-ullah <mdhasanullah124@gmail.com>
2022-03-23 07:16:10 +0600

    nav add

--- my-app/src/Components/Header/Header.js
+++ my-app/src/Components/Header/Header.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import logo from '../../images/Logo.svg'
+import './Header.css'
 
 const Header = () => {
     return (
-        <div>
-            <h2>this is header</h2>
-        </div>
+        <nav className='header'>
+            <img src={logo} alt="" />
+            <div>
+                <a href="">home</a>
+                <a href="">service</a>
+                <a href="">about</a>
+                <a href="">inventory</a>
+            </div>
+        </nav>
     );
 };
 
